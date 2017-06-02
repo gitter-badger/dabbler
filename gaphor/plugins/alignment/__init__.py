b1ee2129aeff3cebc296c3b8d2ced4814b6576e2
Dan Yeaw <dan@yeaw.me>
2017-06-01 21:29:51 -0400

    Reformat for pep8

--- gaphor/plugins/alignment/__init__.py
+++ gaphor/plugins/alignment/__init__.py
@@ -23,35 +23,36 @@ This plugin extends Gaphor with XMI alignment actions.
 """
 
 from __future__ import absolute_import
+
 from zope import interface, component
+
 from gaphor.core import inject, transactional, action, build_action_group
 from gaphor.interfaces import IService, IActionProvider
 from gaphor.ui.interfaces import IDiagramSelectionChange
 
 
 class Alignment(object):
-
     interface.implements(IService, IActionProvider)
 
     component_registry = inject('component_registry')
 
     menu_xml = """
-      <ui>
-        <menubar action="mainwindow">
-          <menu action="diagram">
-            <placeholder name="ternary">
-		<separator />
-		<menuitem action="align-left" />
-		<menuitem action="align-center" />
-		<menuitem action="align-right" />
-		<separator />
-		<menuitem action="align-top" />
-		<menuitem action="align-middle" />
-		<menuitem action="align-bottom" />
-            </placeholder>
-          </menu>
-        </menubar>
-      </ui>"""
+        <ui>
+            <menubar action="mainwindow">
+                <menu action="diagram">
+                    <placeholder name="ternary">
+            <separator />
+            <menuitem action="align-left" />
+            <menuitem action="align-center" />
+            <menuitem action="align-right" />
+            <separator />
+            <menuitem action="align-top" />
+            <menuitem action="align-middle" />
+            <menuitem action="align-bottom" />
+                    </placeholder>
+                </menu>
+            </menubar>
+        </ui>"""
 
     def __init__(self):
         self.action_group = build_action_group(self)
@@ -59,7 +60,7 @@ class Alignment(object):
 
     def init(self, app):
         self.component_registry.register_handler(self.update)
-    
+
     def shutdown(self):
         self.component_registry.unregister_handler(self.update)
 
@@ -67,27 +68,28 @@ class Alignment(object):
     def update(self, event=None):
         self._last_update = event
         sensitive = event and len(event.selected_items) > 1
-	self.action_group.get_action('align-left').set_sensitive(sensitive)
-	self.action_group.get_action('align-center').set_sensitive(sensitive)
-	self.action_group.get_action('align-right').set_sensitive(sensitive)
-	self.action_group.get_action('align-top').set_sensitive(sensitive)
-	self.action_group.get_action('align-middle').set_sensitive(sensitive)
-	self.action_group.get_action('align-bottom').set_sensitive(sensitive)
+        self.action_group.get_action('align-left').set_sensitive(sensitive)
+        self.action_group.get_action('align-center').set_sensitive(sensitive)
+        self.action_group.get_action('align-right').set_sensitive(sensitive)
+        self.action_group.get_action('align-top').set_sensitive(sensitive)
+        self.action_group.get_action('align-middle').set_sensitive(sensitive)
+        self.action_group.get_action('align-bottom').set_sensitive(sensitive)
 
     def get_items(self):
         return (self._last_update and self._last_update.selected_items) or []
+
     def get_focused_item(self):
         return self._last_update.focused_item
-            
+
     def getXCoordsLeft(self, items):
         return [item.matrix[4] for item in items]
-	
+
     def getXCoordsRight(self, items):
         return [item.matrix[4] + item.width for item in items]
 
     def getYCoordsTop(self, items):
         return [item.matrix[5] for item in items]
-	
+
     def getYCoordsBottom(self, items):
         return [item.matrix[5] + item.height for item in items]
 
@@ -97,36 +99,36 @@ class Alignment(object):
     def align_left(self):
         items = self.get_items()
         fitem = self.get_focused_item()
-        target_x= fitem.matrix[4]
+        target_x = fitem.matrix[4]
         for item in items:
             x = target_x - item.matrix[4]
-            item.matrix.translate(x,0)
+            item.matrix.translate(x, 0)
             item.request_update()
-	
+
     @action(name='align-center', label='Center',
             tooltip="Vertically align diagram elements on their centers", accel='<control><shift>c')
     @transactional
     def align_center(self):
         items = self.get_items()
         fitem = self.get_focused_item()
-        min_x=min(self.getXCoordsLeft(items))
-	max_x=max(self.getXCoordsRight(items))
-	center_x = fitem.matrix[4] + (fitem.width / 2)
+        min_x = min(self.getXCoordsLeft(items))
+        max_x = max(self.getXCoordsRight(items))
+        center_x = fitem.matrix[4] + (fitem.width / 2)
         for item in items:
             x = center_x - (item.width / 2) - item.matrix[4]
-            item.matrix.translate(x,0)
+            item.matrix.translate(x, 0)
             item.request_update()
-	
+
     @action(name='align-right', label='Right',
             tooltip="Vertically align diagram elements on the right", accel='<control><shift>r')
     @transactional
     def align_right(self):
         items = self.get_items()
         fitem = self.get_focused_item()
-        target_x= fitem.matrix[4] + fitem.width
+        target_x = fitem.matrix[4] + fitem.width
         for item in items:
             x = target_x - item.width - item.matrix[4]
-            item.matrix.translate(x,0)
+            item.matrix.translate(x, 0)
             item.request_update()
 
     @action(name='align-top', label='Top',
@@ -138,9 +140,9 @@ class Alignment(object):
         target_y = fitem.matrix[5]
         for item in items:
             y = target_y - item.matrix[5]
-            item.matrix.translate(0,y)
+            item.matrix.translate(0, y)
             item.request_update()
-	    
+
     @action(name='align-middle', label='Middle',
             tooltip="Horizontally align diagram elements on their middles", accel='<control><shift>m')
     @transactional
@@ -149,10 +151,10 @@ class Alignment(object):
         fitem = self.get_focused_item()
         middle_y = fitem.matrix[5] + (fitem.height / 2)
         for item in items:
-	    y = middle_y - (item.height / 2) - item.matrix[5]
-            item.matrix.translate(0,y)
+            y = middle_y - (item.height / 2) - item.matrix[5]
+            item.matrix.translate(0, y)
             item.request_update()
-	    
+
     @action(name='align-bottom', label='Bottom',
             tooltip="Horizontally align diagram elements on their bottoms", accel='<control><shift>b')
     @transactional
@@ -162,9 +164,7 @@ class Alignment(object):
         target_y = fitem.matrix[5] + fitem.height
         for item in items:
             y = target_y - item.height - item.matrix[5]
-            item.matrix.translate(0,y)
+            item.matrix.translate(0, y)
             item.request_update()
-	
-
 
 # vim:sw=4:et:ai
